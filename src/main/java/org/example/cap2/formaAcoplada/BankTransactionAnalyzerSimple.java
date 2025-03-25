b4139e2467a1c461bd80dff1a87929afb9862d82
Joao V Chaves <chavesprogrammer@gmail.com>
2025-03-25 18:22:26 -0300

    adição de pegar transações no mês de janeiro ou qualquer mês

--- src/main/java/org/example/cap2/formaAcoplada/BankTransactionAnalyzerSimple.java
+++ src/main/java/org/example/cap2/formaAcoplada/BankTransactionAnalyzerSimple.java
@@ -5,10 +5,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
-    private static final String RESOURCES = "C:\\Users\\joaov\\IdeaProjects\\DSRJ\\src\\main\\java\\org\\example\\cap2\\transacoes.csv";
+    private static final String RESOURCES = "C:\\Users\\joaov\\IdeaProjects\\DSRJ\\src\\main\\java\\org\\example\\cap2\\formaAcoplada\\transacoes.csv";
 
     public static void main(String[] args) throws IOException {
 
@@ -16,18 +19,35 @@ public class BankTransactionAnalyzerSimple {
 
         final List<String> lines = Files.readAllLines(path);
 
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
         double total = 0d;
+
+        double total2 = 0d;
 
         for(final String line: lines){
             final String[] columns = line.split(",");
 
             final double amount = Double.parseDouble(columns[1]);
 
+            final LocalDate date = LocalDate.parse(
+                    columns[0], dateTimeFormatter
+            );
+
+            if (date.getMonth() == Month.JANUARY){
+                final double amountMont = Double.parseDouble(columns[1]);
+                total2 += amountMont;
+            }
+
+
+
             total += amount;
 
         }
 
+
         System.out.println("O total para todas transações é : " + total);
+        System.out.println("O total para todas transações em Janeiro foi : " + total2);
 
     }
 
